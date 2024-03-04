ec46694efac7f9d06ac2a0a8f8d7f0db0785b196
MKarczewski15 <marekkarczewski05@gmail.com>
2024-03-04 13:50:14 +0100

    zmienne

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
+// wyświetlenie alertu na stronie 
 alert("siema");
+
+// wypisanie czegoś w konsoli
 console.log("siema");
+
+// tworzenie zmiennych
+const firstName = "Marek";
+const age = "16"
+
+console.log(firstName);
+console.log(age);
+
+console.log(`mam na imie ${firstName} i mam ${age} lat`);
