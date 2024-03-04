ccdff29230155b114afa76442c7a6576003c6c3c
MKarczewski15 <marekkarczewski05@gmail.com>
2024-03-04 14:05:59 +0100

    powtórka

--- script.js
+++ script.js
@@ -12,3 +12,29 @@ console.log(firstName);
 console.log(age);
 
 console.log(`mam na imie ${firstName} i mam ${age} lat`);
+
+class kalkulator {
+    static wynik(a, b, c) {
+        if (c === '+'){
+            return a + b;
+        } else if (c === '-') {
+            return a - b;
+        } else if (c === '*') {
+            return a * b;
+        } else if (c === '/') {
+            if (b === 0) {
+                console.log("nie można dzielićprzez 0");
+            } else {
+                return a / b;
+            }
+        } else {
+            console.log("niepoprawny znak");
+        }
+    }
+    
+    static oblicz(a, b, c) {
+        return this.wynik(a, b, c);
+    }
+}
+
+console.log(kalkulator.oblicz(12, 34, "/"));
